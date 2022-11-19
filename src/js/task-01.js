a63e74f56a0df59e85e49ca64f2b8483a1a761ca
marlena320 <marlena_duleba@o2.pl>
2022-11-19 15:44:09 +0100

    task-01 done

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+
+const categoriesArray = document.querySelectorAll(".item");
+const categoriesNumber = categoriesArray.length;
+console.log(`Number of categories: ${categoriesNumber}`);
+
+console.log("");
+
+categoriesArray.forEach(element => {
+    const list = element.querySelectorAll("li").length;
+    const name = element.querySelector("h2").textContent;
+    console.log(`Category: ${name}`)
+    console.log(`Elements: ${list}`);
+    console.log("");
+});
+
+
+
+
+
