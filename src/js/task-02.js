439dc3013c9a1d1eb29e017809dc23ebf1d8d5c9
marlena320 <marlena_duleba@o2.pl>
2022-11-19 16:25:48 +0100

    task-02 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const addItems = ingredients.forEach(element => {
+  const item = document.createElement("li");
+  item.textContent = element;
+  item.classList.add("item");
+  list.append(item);
+});
