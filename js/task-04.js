07f7334d30a03123940d482b2c673dd103f253d9
marlena320 <marlena_duleba@o2.pl>
2022-11-20 22:04:15 +0100

    task-04 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,15 @@
-const 
+const btnDecrement = document.querySelector(`[data-action="decrement"]`);
+const btnIncrement = document.querySelector(`[data-action="increment"]`);
+const currentValue = document.querySelector("#value");
+
+let counterValue = 0;
+
+btnDecrement.addEventListener("click", () => {
+    counterValue -= 1;
+    currentValue.textContent = counterValue;
+})
+    
+btnIncrement.addEventListener("click", () => {
+    counterValue += 1;
+    currentValue.textContent = counterValue;
+})
