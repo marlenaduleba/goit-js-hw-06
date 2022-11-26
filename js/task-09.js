a6388d95e3c66e51f3b469dd4832eb44088c120c
marlena320 <marlena_duleba@o2.pl>
2022-11-26 12:17:17 +0100

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", () => {
+  color.textContent = getRandomHexColor();
+  body.style.backgroundColor = color.textContent;
+});
