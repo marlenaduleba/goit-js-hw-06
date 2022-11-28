378d6cc172d08f84ce00ad994c074f4963b6bfd6
marlena320 <marlena_duleba@o2.pl>
2022-11-28 18:11:18 +0100

    indicator set to minimum

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,8 @@
 const range = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
+const { min } = range;
+
+range.value = min;
 
 range.addEventListener("input", () => {
   let size = range.value;
